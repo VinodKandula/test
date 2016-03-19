27032d66eac6a788ae76143f8dc0d8a09f32bf3a
Vinod Kandula <vinod.kandula@toovia.com>
2016-03-20 01:18:44 +0530

    Test

--- com/test/MaxSubsetSumFinder.java
+++ com/test/MaxSubsetSumFinder.java
@@ -6,7 +6,29 @@ package com.test;
  */
 public class MaxSubsetSumFinder {
 	
-	private static int ofNonConsecutiveElements (int... elements) {
+	private static int findNonConsecutiveElementsMaxSum(int... elements) {
+	    int maxsofar,currentSum,previousSum;
+
+	    maxsofar = currentSum = previousSum = 0;
+	    boolean isFirst = true;
+	    for (int i = 0; i < elements.length; i++) {
+	    	for (int j=i+2; j<elements.length; j++) {
+	    		if (isFirst) {
+	    			maxsofar = elements[i] + elements[j];
+	    			previousSum =  maxsofar;
+	    			isFirst = false;
+	    			continue;
+	    		} else {
+	    			maxsofar =  Math.max(elements[i] + elements[j], previousSum);
+	    		}
+	    		previousSum = maxsofar;
+	    	}
+	    }
+	    int lastMax = Math.max(elements[elements.length-2],  elements[elements.length-1]);
+	    return Math.max(lastMax,maxsofar);        
+	}
+	
+	private static int ofNonConsecutiveElements(int... elements) {
 	    int maxsofar,maxi2,maxi1;
 
 	    maxi1 = maxsofar = elements[0];
@@ -17,11 +39,15 @@ public class MaxSubsetSumFinder {
 	        maxi2 =  maxi1;
 	        maxi1 = maxsofar;
 	    }
-	    return maxsofar;        
+	    int lastMax = Math.max(elements[elements.length-2],  elements[elements.length-1]);
+	    return Math.max(lastMax,maxsofar);        
 	}
 
 	public static void main(String args[]) {
-	    System.out.println(ofNonConsecutiveElements(9, -17, 11, 20, 15, 3, -40));
+		//O(n2)
+		//System.out.println(findNonConsecutiveElementsMaxSum(-9, -17, -11, -20, -15, -3, -6650));
+		//O(n)
+	    System.out.println(ofNonConsecutiveElements(-9, -17, -11, -20, -15, -3, -6650, -1));
 	}
 
 }
